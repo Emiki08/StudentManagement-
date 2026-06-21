a40f3f4da1c55bf18962c6071a251a53e154169c
Emiki08 <me8me8i5529@gmail.com>
2026-06-21 19:48:24 +0900

    Add student courses search

--- src/main/java/raisetech/StudentManagement/Student.java
+++ src/main/java/raisetech/StudentManagement/Student.java
@@ -1,22 +1,20 @@
 package raisetech.StudentManagement;
 
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import  lombok.Getter;
+import  lombok.Setter;
+
+@Getter
+@Setter
+
 public class Student {
+
+  private  String id;
   private  String name;
-  private int age;
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public int getAge() {
-    return age;
-  }
-
-  public void setAge(int age) {
-    this.age = age;
-  }
+  private  String furigana;
+  private  String nickname;
+  private  String email;
+  private  String area;
+  private  int age;
+  private  String gender;
 }
